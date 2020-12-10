8b4a6066d0d744e155b2fa120a1821d11c8214f7
Manwinder Singh <manwindersingh1712@gmail.com>
2020-12-10 20:47:53 +0530

    included the active class handler

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,16 @@ import React from "react";
 import "../utils/css/header.css";
 import logo from "../utils/images/logo.png";
 class Header extends React.Component {
+  state = { home: true, about: false, explore: false };
+  homeActive = () => {
+    this.setState({ home: true, about: false, explore: false });
+  };
+  aboutActive = () => {
+    this.setState({ home: false, about: true, explore: false });
+  };
+  exploreActive = () => {
+    this.setState({ home: false, about: false, explore: true });
+  };
   render() {
     return (
       <div className="header_container">
@@ -10,9 +20,24 @@ class Header extends React.Component {
         </div>
         <div className="links">
           <ul>
-            <li>Home</li>
-            <li>About Me</li>
-            <li>Explore</li>
+            <li
+              className={this.state.home ? "active" : ""}
+              onClick={this.homeActive}
+            >
+              Home
+            </li>
+            <li
+              className={this.state.about ? "active" : ""}
+              onClick={this.aboutActive}
+            >
+              About Me
+            </li>
+            <li
+              className={this.state.explore ? "active" : ""}
+              onClick={this.exploreActive}
+            >
+              Explore
+            </li>
 
             <a href="">
               <i class="fab fa-instagram"></i>
