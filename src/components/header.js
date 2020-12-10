f081de1659528dc201aa2689ad9f7be2087eecca
Manwinder Singh <manwindersingh1712@gmail.com>
2020-12-10 19:27:29 +0530

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../utils/css/header.css";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className="header_container">
+        <div></div>
+      </div>
+    );
+  }
+}
+
+export default Header;
